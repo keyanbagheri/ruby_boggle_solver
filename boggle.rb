acb08a8bf6ce314645ca7ceadfad8473bbaf4d79
keyanbagheri <keyanbagheri@gmail.com>
2016-03-18 14:47:13 -0700

    creating gameboard

--- boggle.rb
+++ boggle.rb
@@ -1,7 +1,15 @@
 class Boggle
 
   def initialize
+    # seed a small list of words to check from
     @word_list = ["art", "bet", "cat", "catch", "her", "ruby", "hyfn"]
+    # seed a default board to check
+    @board = [
+              ['b', 'e', 't', 'x'],
+              ['a', 'r', 'c', 'a'],
+              ['r', 'c', 't', 'x'],
+              ['e', 'h', 's', 'i']
+             ]
   end
 
   def check_word(string)
